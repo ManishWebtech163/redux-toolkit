f51c97956804141c4713d349d9f947260e748dce
ManishWebtech163 <panchalmanish163@gmail.com>
2023-04-06 16:50:30 +0530

    redux toolkit complete

--- src/components/DataPage.jsx
+++ src/components/DataPage.jsx
@@ -6,8 +6,14 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import { Button, Typography } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import Chance from 'chance';
+import { addUser, clearAllUsers, removeUser } from '../store/slices/userSlice';
 
 export default function DataPage() {
+    let chance = Chance()
+    const dispatch = useDispatch()
+
     const Item = styled(Paper)(({ theme }) => ({
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
         ...theme.typography.body2,
@@ -15,47 +21,56 @@ export default function DataPage() {
         textAlign: 'center',
         color: theme.palette.text.secondary,
     }));
+
+    //   ---functions---
+    //add user
+    const addUserFun = () => {
+        let userCreate = `${chance.first()} ${chance.last()}`;
+        dispatch(addUser(userCreate))
+    }
+
+    // delete user
+    const deleteUser = (e) => {
+        dispatch(removeUser(e))
+    }
+
+    //clearAllUsers
+    const clearAll_Users = () => {
+        dispatch(clearAllUsers())
+    }
+
+
+    // --access store data--
+    const storeData = useSelector(state => state.users)
+    console.log(storeData)
+
     return (
         <React.Fragment>
             <Container maxWidth="lg">
                 <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', mt: 2, p: 2 }} >
                     <marquee direction="right">  <Typography sx={{ fontSize: "2rem", }}> Redux Toolkit </Typography> </marquee>
-                    <Button>Add Item</Button>
+                    <Button onClick={addUserFun}>Add Item</Button>
                     <Grid container spacing={2}>
                         {/* --data-- */}
-                        <Grid xs={12}>
-                            <Item>
-                                <Grid container>
-                                    <Grid xs={4}>asd</Grid>
-                                    <Grid xs={4}>asd</Grid>
-                                    <Grid xs={2}>asd</Grid>
-                                    <Grid xs={2}> <Button>Delete Item</Button> </Grid>
+                        {storeData && storeData.map((e) => {
+                            return (
+                                <Grid xs={12}>
+                                    <Item>
+                                        <Grid container>
+                                            <Grid xs={4}>{e}</Grid>
+                                            <Grid xs={4}>asd</Grid>
+                                            <Grid xs={2}>asd</Grid>
+                                            <Grid xs={2}> <Button onClick={() => deleteUser(e)}>Delete Item</Button> </Grid>
+                                        </Grid>
+                                    </Item>
                                 </Grid>
-                            </Item>
-                        </Grid>
-                        <Grid xs={12}>
-                            <Item>
-                                <Grid container>
-                                    <Grid xs={4}>asd</Grid>
-                                    <Grid xs={4}>asd</Grid>
-                                    <Grid xs={2}>asd</Grid>
-                                    <Grid xs={2}> <Button>Delete Item</Button> </Grid>
-                                </Grid>
-                            </Item>
-                        </Grid>
-                        <Grid xs={12}>
-                            <Item>
-                                <Grid container>
-                                    <Grid xs={4}>asd</Grid>
-                                    <Grid xs={4}>asd</Grid>
-                                    <Grid xs={2}>asd</Grid>
-                                    <Grid xs={2}> <Button>Delete Item</Button> </Grid>
-                                </Grid>
-                            </Item>
-                        </Grid>
+                            )
+                        })}
+
+
                         {/* --//data-- */}
                     </Grid>
-                    <Button>Clear All Item</Button>
+                    <Button onClick={() => clearAll_Users()}>Clear All Item</Button>
                 </Box>
             </Container>
         </React.Fragment>
---
+++ src/store/slices/userSlice.jsx
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const usersSlice = createSlice({
+    name: "user",
+    initialState: [],
+    reducers: {
+        // add user
+        addUser(state, action) {
+            state.push(action.payload)
+        },
+        // remove user
+        removeUser(state, action) {
+            let userIndex = state.indexOf(action.payload)
+            state.splice(userIndex, 1)
+        },
+        // clear user
+        clearAllUsers(state, action) {
+            return []
+        } 
+    }
+})
+
+export default usersSlice;
+export const { addUser, removeUser, clearAllUsers } = usersSlice.actions;
